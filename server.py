86bc777f96ff09b5be48518d9ceed64146bdcb94
Ashley Jelks <arjelks@gmail.com>
2015-10-28 11:40:11 -0700

    secured keys

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+import secrets
 
 
 app = Flask(__name__)
-app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/', methods=['GET'])
-def homepage():
+def view_index():
 
-    """Wanderlust Homepage. Users can register for an account, login, or simply search flights."""
+   """Route to view drafts of github.io page"""
 
     return render_template("index.html")
 
@@ -19,6 +19,6 @@ def homepage():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     DebugToolbarExtension(app)
     app.run()
